d715d2255b370fb93782793a83fd05029c445ff3
frantracer <frantracer@gmail.com>
2025-07-13 17:08:55 +0200

    Create handler to create new group

---
+++ group_sms_chat/application/create_new_group_handler.py
@@ -0,0 +1,70 @@
+from group_sms_chat.domain.exceptions import MaximumNumberOfGroupsReachedError
+from group_sms_chat.domain.group import Group, GroupName
+from group_sms_chat.domain.group_repository import GroupRepository
+from group_sms_chat.domain.sms_service import SMSService
+from group_sms_chat.domain.user import PhoneNumber, User
+
+
+class CreateNewGroupHandler:
+    def __init__(self, group_repository: GroupRepository, sms_service: SMSService) -> None:
+        """
+        Initialize the CreateNewGroupHandler.
+
+        :param group_repository: An instance of GroupRepository to manage group data.
+        :param sms_service: An instance of SMSService to handle SMS operations.
+        """
+        self.group_repository = group_repository
+        self.sms_service = sms_service
+
+    async def handle(self, group_name: GroupName, user: User) -> Group:
+        """
+        Handle the creation of a new group.
+
+        :param group_name: The name of the group to be created.
+        :param user: The user who is creating the group.
+        :return: The created group or an error message.
+        """
+        available_phone_numbers = await self.sms_service.available_phone_numbers()
+
+        current_groups = await self.group_repository.find_groups_by_name(group_name)
+
+        if len(current_groups) >= len(available_phone_numbers):
+            raise MaximumNumberOfGroupsReachedError(username=user.username, max_number=len(available_phone_numbers))
+
+        used_phone_numbers = [group.get_user_phone_number(user.username) for group in current_groups]
+
+        phone_number = find_available_phone_number(
+            used_numbers=[phone_number for phone_number in used_phone_numbers if phone_number is not None],
+            available_numbers=available_phone_numbers
+        )
+        if phone_number is None:
+            raise MaximumNumberOfGroupsReachedError(username=user.username, max_number=len(available_phone_numbers))
+
+        group = Group(name=group_name)
+        group.add_user(user.username, phone_number)
+
+        await self.group_repository.create_or_update_group(group)
+
+        await self.sms_service.send_sms(
+            from_phone_number=phone_number,
+            to_phone_number=user.phone_number,
+            message=f"You have created the group '{group_name}'. "
+                    f"Reply to this message to send text messages to the members of the group.")
+
+        return group
+
+
+def find_available_phone_number(
+        used_numbers: list[PhoneNumber],
+        available_numbers: list[PhoneNumber]
+) -> PhoneNumber | None:
+    """
+    Find an available phone number from the list of available phone numbers.
+    :param used_numbers: A list of phone numbers that are already in use.
+    :param available_numbers: A list of available phone numbers.
+    :return: An available phone number or None if no available number is found.
+    """
+    for number in available_numbers:
+        if number not in used_numbers:
+            return number
+    return None
--- group_sms_chat/domain/exceptions.py
+++ group_sms_chat/domain/exceptions.py
@@ -16,6 +16,11 @@ class PhoneNumberAlreadyExistsError(Exception):
         super().__init__(f"User with phone number '{phone_number}' already exists.")
 
 
+class MaximumNumberOfGroupsReachedError(Exception):
+    def __init__(self, username: Username, max_number: int) -> None:
+        super().__init__(f"User '{username}' has reached the maximum number {max_number} of groups they can join.")
+
+
 class UserInvalidCredentialsError(Exception):
     def __init__(self, username: Username) -> None:
         super().__init__(f"Invalid credentials for user '{username}'. Please check your username and password.")
